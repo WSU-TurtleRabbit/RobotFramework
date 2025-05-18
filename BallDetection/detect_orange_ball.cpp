6c662392f431851d4145421ef29dfa48b7ce9fe9
Jason Ng <7614175+jsnng@users.noreply.github.com>
2025-05-18 11:26:57 +1000

    format detect_orange_ball.cpp

--- BallDetection/detect_orange_ball.cpp
+++ BallDetection/detect_orange_ball.cpp
@@ -1,25 +1,16 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include "detect_ball.h"
-#include <chrono>
-
 
 int main() {
     BallDetection b;
 
-    while(true){
-    auto start_time = std::chrono::high_resolution_clock::now();
-    bool detected = b.find_ball();
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-        std::cout << "Execution time: " << duration.count() << " microseconds" << std::endl;
-    sleep(1);
-    std::cout << detected << std::endl;
+    while(true) {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        bool detected = b.find_ball();
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+            std::cout << "Execution time: " << duration.count() << " microseconds" << std::endl;
+        sleep(1);
+        std::cout << detected << std::endl;
     };
     
     return 0;
