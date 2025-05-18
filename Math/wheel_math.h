4888955efb3a31f06a599bb80e6e41449b53a82c
Jason Ng <7614175+jsnng@users.noreply.github.com>
2025-05-18 11:27:49 +1000

    remove deg2rad, using constant

--- Math/wheel_math.h
+++ Math/wheel_math.h
@@ -8,30 +8,38 @@
 class Wheel_math{
     private:
 
-    const std::vector<double> wheel_1 = {63.6, 36.87};
-    const std::vector<double> wheel_2 = {52.14, -52.14};
-    const std::vector<double> wheel_3 = {-52.14, -52.14};
-    const std::vector<double> wheel_4 = {-63.86, 36.87};
+    const std::vector<double> WHEEL_1 = {63.6, 36.87};
+    const std::vector<double> WHEEL_2 = {52.14, -52.14};
+    const std::vector<double> WHEEL_3 = {-52.14, -52.14};
+    const std::vector<double> WHEEL_4 = {-63.86, 36.87};
 
-    const int wheel_1_ang = -120;
-    const int wheel_2_ang = -45;
-    const int wheel_3_ang = 45;
-    const int wheel_4_ang = 120;
+    const int WHEEL_1_ANG = -120;
+    const int WHEEL_2_ANG = -45;
+    const int WHEEL_3_ANG = 45;
+    const int WHEEL_4_ANG = 120;
 
-    const double radius = 33.5;
+    //todo - convert to radians
 
-    
+    const int WHEEL_1_RAD = WHEEL_1_ANG/(M_PI/180);
+    const int WHEEL_2_RAD = WHEEL_2_ANG/(M_PI/180);
+    const int WHEEL_3_RAD = WHEEL_3_ANG/(M_PI/180);
+    const int WHEEL_4_RAD = WHEEL_4_ANG/(M_PI/180);
 
-    float wheel_dist_1 = sqrt(pow(wheel_1[0],2) + pow(wheel_1[1],2));
-    float wheel_dist_2 = sqrt(pow(wheel_2[0],2) + pow(wheel_2[1],2));
-    float wheel_dist_3 = sqrt(pow(wheel_3[0],2) + pow(wheel_3[1],2));
-    float wheel_dist_4 = sqrt(pow(wheel_4[0],2) + pow(wheel_4[1],2));
+    const double WHEEL_RADIUS = 33.5;
+
+    float wheel_dist_1 = sqrt(pow(WHEEL_1[0],2) + pow(WHEEL_1[1],2));
+    float wheel_dist_2 = sqrt(pow(WHEEL_2[0],2) + pow(WHEEL_2[1],2));
+    float wheel_dist_3 = sqrt(pow(WHEEL_3[0],2) + pow(WHEEL_3[1],2));
+    float wheel_dist_4 = sqrt(pow(WHEEL_4[0],2) + pow(WHEEL_4[1],2));
 
 
     public:
+
+    Wheel_math() = default;
+    ~Wheel_math() = default;
     // SetWheelDist();
     std::vector<double> calculate(double velocity_x, double velocity_y, double velocity_w);
-    double deg2rad(int angle);
+    // double deg2rad(int angle);
 
 };
 
--- Math/wheel_velocity.cpp
+++ Math/wheel_velocity.cpp
@@ -3,15 +3,15 @@
 #include <cmath>
 #include "wheel_math.h"
 
-double Wheel_math::deg2rad(int angle){
-    return angle * M_PI/180;
-}
+// double Wheel_math::deg2rad(int angle){
+//     return angle * M_PI/180;
+// }
 
 std::vector<double> Wheel_math::calculate(double velocity_x, double velocity_y, double velocity_w){
     std::vector<double> wheel_vel(4);
-    wheel_vel[0] = {(1/radius)*(wheel_dist_1*velocity_w) - (velocity_x * sin(deg2rad(wheel_1_ang)))+ (velocity_y * cos(deg2rad(wheel_1_ang)))};
-    wheel_vel[1] = {(1/radius)*(wheel_dist_2*velocity_w) - (velocity_x * sin(deg2rad(wheel_2_ang)))+ (velocity_y * cos(deg2rad(wheel_2_ang)))};
-    wheel_vel[2] = {(1/radius)*(wheel_dist_3*velocity_w) - (velocity_x * sin(deg2rad(wheel_3_ang)))+ (velocity_y * cos(deg2rad(wheel_3_ang)))};
-    wheel_vel[3] = {(1/radius)*(wheel_dist_4*velocity_w) - (velocity_x * sin(deg2rad(wheel_4_ang)))+ (velocity_y * cos(deg2rad(wheel_4_ang)))}; 
+    wheel_vel[0] = {(1/WHEEL_RADIUS)*(wheel_dist_1*velocity_w) - (velocity_x * sin(WHEEL_1_RAD))+ (velocity_y * cos(WHEEL_1_RAD))};
+    wheel_vel[1] = {(1/WHEEL_RADIUS)*(wheel_dist_2*velocity_w) - (velocity_x * sin(WHEEL_2_RAD))+ (velocity_y * cos(WHEEL_2_RAD))};
+    wheel_vel[2] = {(1/WHEEL_RADIUS)*(wheel_dist_3*velocity_w) - (velocity_x * sin(WHEEL_3_RAD))+ (velocity_y * cos(WHEEL_3_RAD))};
+    wheel_vel[3] = {(1/WHEEL_RADIUS)*(wheel_dist_4*velocity_w) - (velocity_x * sin(WHEEL_4_RAD))+ (velocity_y * cos(WHEEL_4_RAD))}; 
     return wheel_vel;
 }
