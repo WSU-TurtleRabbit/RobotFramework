8cd32b5b373b6ab60c2bb3f5f57a12fb73e77ade
AliveDPG <darpangurung187@gmail.com>
2025-07-11 15:27:25 +1000

    1/r now multiplies with the whole value now

--- Math/wheel_velocity.cpp
+++ Math/wheel_velocity.cpp
@@ -9,9 +9,9 @@
 
 std::vector<double> Wheel_math::calculate(double velocity_x, double velocity_y, double velocity_w){
     std::vector<double> wheel_vel(4);
-    wheel_vel[0] = {(1/WHEEL_RADIUS)*(wheel_dist_1*velocity_w) - (velocity_x * sin(WHEEL_1_RAD))+ (velocity_y * cos(WHEEL_1_RAD))};
-    wheel_vel[1] = {(1/WHEEL_RADIUS)*(wheel_dist_2*velocity_w) - (velocity_x * sin(WHEEL_2_RAD))+ (velocity_y * cos(WHEEL_2_RAD))};
-    wheel_vel[2] = {(1/WHEEL_RADIUS)*(wheel_dist_3*velocity_w) - (velocity_x * sin(WHEEL_3_RAD))+ (velocity_y * cos(WHEEL_3_RAD))};
-    wheel_vel[3] = {(1/WHEEL_RADIUS)*(wheel_dist_4*velocity_w) - (velocity_x * sin(WHEEL_4_RAD))+ (velocity_y * cos(WHEEL_4_RAD))}; 
+    wheel_vel[0] = {(1/WHEEL_RADIUS)*((wheel_dist_1*velocity_w) - (velocity_x * sin(WHEEL_1_RAD))+ (velocity_y * cos(WHEEL_1_RAD)))};
+    wheel_vel[1] = {(1/WHEEL_RADIUS)*((wheel_dist_2*velocity_w) - (velocity_x * sin(WHEEL_2_RAD))+ (velocity_y * cos(WHEEL_2_RAD)))};
+    wheel_vel[2] = {(1/WHEEL_RADIUS)*((wheel_dist_3*velocity_w) - (velocity_x * sin(WHEEL_3_RAD))+ (velocity_y * cos(WHEEL_3_RAD)))};
+    wheel_vel[3] = {(1/WHEEL_RADIUS)*((wheel_dist_4*velocity_w) - (velocity_x * sin(WHEEL_4_RAD))+ (velocity_y * cos(WHEEL_4_RAD)))}; 
     return wheel_vel;
 }
