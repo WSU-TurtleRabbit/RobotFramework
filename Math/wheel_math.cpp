5f774487f5227aeb12a0bd858b78aade87571ef8
AliveDPG <darpangurung187@gmail.com>
2025-12-06 19:53:04 +1100

    Wheel maths have been fixed and fixed implementataion of limits

--- Math/wheel_math.cpp
+++ Math/wheel_math.cpp
@@ -3,30 +3,44 @@
 #include <cmath>
 #include "wheel_math.h"
 
-// double Wheel_math::deg2rad(int angle){
-//     return angle * M_PI/180;
-// }
+Wheel_math::Wheel_math() {
 
-std::vector<double> Wheel_math::calculate(double velocity_x, double velocity_y, double velocity_w){
-      std::vector<double> stop_vel(4);
-      for(int i = 0; i< 3; i++)
-        stop_vel[i] = 0.0;
-    if(std::abs(velocity_x) > 0.5){
-        std::cout << "error:incomming x is too large" << "\n";
+}
+
+std::vector<double> Wheel_math::calculate(double velocity_x, double velocity_y, double velocity_w) {
+
+    // ---- Limit Checking ----
+    if (std::abs(velocity_x) > X_LIMIT) {
+        std::cout << "error: incoming x is too large\n";
         return stop_vel;
     }
-    if(std::abs(velocity_y) > 0.5){
-        std::cout << "error:incomming y is too large" << "\n";
+    if (std::abs(velocity_y) > Y_LIMIT) {
+        std::cout << "error: incoming y is too large\n";
         return stop_vel;
     }
-    if(std::abs(velocity_w) > 0.1){
-        std::cout << "error:incomming w is too large" << "\n";
+    if (std::abs(velocity_w) > W_LIMIT) {
+        std::cout << "error: incoming w is too large\n";
         return stop_vel;
     }
-    std::vector<double> wheel_vel(4);
-    wheel_vel[0] = {(1/WHEEL_RADIUS) * ((wheel_dist_1*velocity_w) - (velocity_x * sin(WHEEL_1_RAD)) + (velocity_y * cos(WHEEL_1_RAD)))};
-    wheel_vel[1] = {(1/WHEEL_RADIUS) * ((wheel_dist_2*velocity_w) - (velocity_x * sin(WHEEL_2_RAD)) + (velocity_y * cos(WHEEL_2_RAD)))};
-    wheel_vel[2] = {(1/WHEEL_RADIUS) * ((wheel_dist_3*velocity_w) - (velocity_x * sin(WHEEL_3_RAD)) + (velocity_y * cos(WHEEL_3_RAD)))};
-    wheel_vel[3] = {(1/WHEEL_RADIUS) * ((wheel_dist_4*velocity_w) - (velocity_x * sin(WHEEL_4_RAD)) + (velocity_y * cos(WHEEL_4_RAD)))}; 
+
+    // ---- Omni Wheel Kinematics ----
+    wheel_vel[0] = ( (velocity_x * std::sin(WHEEL_1_RAD)) +
+                     (velocity_y * std::cos(WHEEL_1_RAD)) +
+                     (velocity_w * wheel_dist_1) ) / WHEEL_RADIUS;
+
+    wheel_vel[1] = ( (velocity_x * std::sin(WHEEL_2_RAD)) +
+                     (velocity_y * std::cos(WHEEL_2_RAD)) +
+                     (velocity_w * wheel_dist_2) ) / WHEEL_RADIUS;
+
+    wheel_vel[2] = ( (velocity_x * std::sin(WHEEL_3_RAD)) +
+                     (velocity_y * std::cos(WHEEL_3_RAD)) +
+                     (velocity_w * wheel_dist_3) ) / WHEEL_RADIUS;
+
+    wheel_vel[3] = ( (velocity_x * std::sin(WHEEL_4_RAD)) +
+                     (velocity_y * std::cos(WHEEL_4_RAD)) +
+                     (velocity_w * wheel_dist_4) ) / WHEEL_RADIUS;
+
     return wheel_vel;
 }
+
+
--- Math/wheel_math.h
+++ Math/wheel_math.h
@@ -1,49 +1,55 @@
 #ifndef WHEEL_MATH_H
 #define WHEEL_MATH_H
-#include <sstream>
-#include <string>
+
 #include <cmath>
 #include <vector>
 
-class Wheel_math{
-    private:
+class Wheel_math {
+private:
+    // Wheel positions in meters (x, y)
+    const std::vector<double> WHEEL_1 = {63.6/1000.0, 36.87/1000.0};
+    const std::vector<double> WHEEL_2 = {52.14/1000.0, -52.14/1000.0};
+    const std::vector<double> WHEEL_3 = {-52.14/1000.0, -52.14/1000.0};
+    const std::vector<double> WHEEL_4 = {-63.6/1000.0, 36.87/1000.0};
 
-    const std::vector<double> WHEEL_1 = {63.6/1000, 36.87/1000};
-    const std::vector<double> WHEEL_2 = {52.14/1000, -52.14/1000};
-    const std::vector<double> WHEEL_3 = {-52.14/1000, -52.14/1000};
-    const std::vector<double> WHEEL_4 = {-63.86/1000, 36.87/1000};
+    // Wheel orientation (degrees)
+    const double W1_ANG = 45.0;
+    const double W2_ANG = -45.0;
+    const double W3_ANG = -120.0;
+    const double W4_ANG = 120.0;
 
-    const int WHEEL_1_ANG = 45;
-    const int WHEEL_2_ANG = -45;
-    const int WHEEL_3_ANG = -120;
-    const int WHEEL_4_ANG = 120;
+    // Stop state
+    std::vector<double> stop_vel = {0.0, 0.0, 0.0, 0.0};
 
+    // Max velocity limits
+    const float X_LIMIT = 0.5;   // m/s
+    const float Y_LIMIT = 0.5;   // m/s
+    const float W_LIMIT = 0.1;   // rad/s
 
+    // Wheel computed velocities
+    std::vector<double> wheel_vel = {0.0, 0.0, 0.0, 0.0};
 
-    //todo - convert to radians
+    // Angles converted to radians
+    const double W1_RAD = W1_ANG * (M_PI / 180.0);
+    const double W2_RAD = W2_ANG * (M_PI / 180.0);
+    const double W3_RAD = W3_ANG * (M_PI / 180.0);
+    const double W4_RAD = W4_ANG * (M_PI / 180.0);
 
-    const double WHEEL_1_RAD = WHEEL_1_ANG*(M_PI/180);
-    const double WHEEL_2_RAD = WHEEL_2_ANG*(M_PI/180);
-    const double WHEEL_3_RAD = WHEEL_3_ANG*(M_PI/180);
-    const double WHEEL_4_RAD = WHEEL_4_ANG*(M_PI/180);
+    // Wheel radius (meters)
+    const double WHEEL_RADIUS = 33.5 / 1000.0;
 
-    const double WHEEL_RADIUS = 33.5/1000;
+    // Distances from robot center
+    float wheel_dist_1 = std::hypot(WHEEL_1[0], WHEEL_1[1]);
+    float wheel_dist_2 = std::hypot(WHEEL_2[0], WHEEL_2[1]);
+    float wheel_dist_3 = std::hypot(WHEEL_3[0], WHEEL_3[1]);
+    float wheel_dist_4 = std::hypot(WHEEL_4[0], WHEEL_4[1]);
 
-    float wheel_dist_1 = sqrt(pow(WHEEL_1[0],2) + pow(WHEEL_1[1],2));
-    float wheel_dist_2 = sqrt(pow(WHEEL_2[0],2) + pow(WHEEL_2[1],2));
-    float wheel_dist_3 = sqrt(pow(WHEEL_3[0],2) + pow(WHEEL_3[1],2));
-    float wheel_dist_4 = sqrt(pow(WHEEL_4[0],2) + pow(WHEEL_4[1],2));
-
-
-    public:
-
+public:
     Wheel_math() = default;
     ~Wheel_math() = default;
-    // SetWheelDist();
+
+    // Calculate wheel velocities from robot desired motion
     std::vector<double> calculate(double velocity_x, double velocity_y, double velocity_w);
-    // double deg2rad(int angle);
-
 };
-
 
 #endif
